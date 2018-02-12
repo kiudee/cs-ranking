09d1f2f2056e5bdfe47e707f576e8664ec01a2d6
Karlson Pfannschmidt <quietdeath@gmail.com>
2018-02-12 13:28:38 +0100

    Update __init__.py

--- csrank/__init__.py
+++ csrank/__init__.py
@@ -1,3 +1,4 @@
 from .fate_ranking import *
 from .objectranking import *
 from .tuning import ParameterOptimizer
+from .dataset_reader.objectranking.synthetic_data_generator import SyntheticDatasetGenerator
