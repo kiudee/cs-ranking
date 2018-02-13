c4568bb7c330b45ee2c48f09901b552da596e8a1
Karlson Pfannschmidt <kiudee@mail.uni-paderborn.de>
2018-02-13 18:06:06 +0100

    Tests for hinged and smooth rank loss

---
+++ csrank/tests/test_losses.py
@@ -0,0 +1,46 @@
+import pytest
+import numpy as np
+from keras import backend as K
+from numpy.testing import assert_almost_equal
+
+from csrank.losses import hinged_rank_loss, smooth_rank_loss
+
+
+def test_hinged_rank_loss():
+    y_true = np.arange(5)[None, :]
+    y_true_tensor = K.constant(y_true)
+
+    # Predicting all 0, gives an error of 1.0:
+    assert_almost_equal(
+        actual=K.eval(
+            hinged_rank_loss(y_true_tensor,
+                             K.constant(np.array([[0., 0., 0., 0., 0.]])))),
+        desired=np.array([1.]))
+
+    # Predicting the correct ranking improves, but penalizes by difference of
+    # scores:
+    assert_almost_equal(
+        actual=K.eval(
+            hinged_rank_loss(y_true_tensor,
+                             K.constant(np.array([[.2, .1, .0, -0.1, -0.2]])))),
+        desired=np.array([0.8]))
+
+
+def test_smooth_rank_loss():
+    y_true = np.arange(5)[None, :]
+    y_true_tensor = K.constant(y_true)
+
+    # Predicting all 0, gives an error of 1.0:
+    assert_almost_equal(
+        actual=K.eval(
+            smooth_rank_loss(y_true_tensor,
+                             K.constant(np.array([[0., 0., 0., 0., 0.]])))),
+        desired=np.array([1.]))
+
+    # Predicting the correct ranking improves, but penalizes by difference of
+    # scores:
+    assert_almost_equal(
+        actual=K.eval(
+            smooth_rank_loss(y_true_tensor,
+                             K.constant(np.array([[.2, .1, .0, -0.1, -0.2]])))),
+        desired=np.array([0.822749841877]))
