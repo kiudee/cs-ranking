4fa70202784eb4870ee320fd3d3f5eb9212c61ae
Karlson Pfannschmidt <kiudee@mail.uni-paderborn.de>
2018-02-13 17:06:10 +0100

    Add napoleon and test first class

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.append(os.path.abspath('..'))
 
 
 # -- General configuration ------------------------------------------------
@@ -33,8 +33,10 @@
 # ones.
 extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
+    'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages']
+    'sphinx.ext.githubpages',
+    'nbsphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
--- docs/index.rst
+++ docs/index.rst
@@ -3,13 +3,18 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to csrank's documentation!
+Welcome to CSRank's documentation!
 ==================================
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
+
+
+.. autoclass:: csrank.FATEObjectRanker
+   :members:
+   :inherited-members:
 
 
 Indices and tables
