afeb7f11f68236ef9f185f5fe09471d0376ea8ea
Karlson Pfannschmidt <kiudee@mail.upb.de>
2018-02-19 11:11:17 +0100

    Add missing package to travis

--- docs/conf.py
+++ docs/conf.py
@@ -77,6 +77,8 @@ language = None
 # This patterns also effect to html_static_path and html_extra_path
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+nbsphinx_execute = 'never'
+
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 
