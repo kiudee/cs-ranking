7084f4a0357e161d5bfb270eb08fdaed2221bd94
Karlson Pfannschmidt <kiudee@mail.uni-paderborn.de>
2018-02-19 14:26:52 +0100

    Small fixes for sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,8 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     'sphinx.ext.autosummary',
-    'nbsphinx']
+    'nbsphinx',
+    'IPython.sphinxext.ipython_console_highlighting']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
