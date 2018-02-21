af33200cc686156a09c728fa7db4531372cb9ea2
Karlson Pfannschmidt <kiudee@mail.uni-paderborn.de>
2018-02-21 17:15:50 +0100

    Implement synthetic iterator & test

--- csrank/tests/test_util.py
+++ csrank/tests/test_util.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 from keras import backend as K
 
 from ..util import tensorify
+from ..dataset_reader.util import SyntheticIterator
 
 
 def test_tensorify():
@@ -13,3 +14,15 @@ def test_tensorify():
     b = K.zeros((5, 3))
     out = tensorify(b)
     assert b == out
+
+
+def test_synthetic_iterator():
+    def func(a, b):
+        return (b, a)
+    it = SyntheticIterator(dataset_function=func,
+                           a=41, b=2)
+    for i, (x, y) in enumerate(it):
+        if i == 1:
+            break
+        assert x == 2
+        assert y == 41
