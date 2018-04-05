e3427cdae5eff2be29bc9a48d76ba45cc90ff215
Karlson Pfannschmidt <kiudee@mail.uni-paderborn.de>
2018-04-05 18:54:26 +0200

    Add threshold tuning

--- csrank/fate_choice.py
+++ csrank/fate_choice.py
@@ -1,8 +1,13 @@
 import logging
 
+import numpy as np
+
 from keras.layers import Dense
 from keras.losses import binary_crossentropy
 from keras.regularizers import l2
+
+from sklearn.metrics import f1_score
+from sklearn.model_selection import train_test_split
 
 from csrank.fate_ranking import FATEObjectRankingCore
 
@@ -29,6 +34,7 @@ class FATEChoiceFunction(FATEObjectRankingCore):
         self.loss_function = loss_function
         self.metrics = metrics
         self.logger = logging.Logger('FATEChoiceFunction')
+        self.threshold = 0.5
 
     def _construct_layers(self, **kwargs):
         """ Construct joint layers and [0,1] output nodes
@@ -57,12 +63,41 @@ class FATEChoiceFunction(FATEObjectRankingCore):
         self.scorer = Dense(1, name="output_node", activation='sigmoid',
                             kernel_regularizer=self.kernel_regularizer)
 
-    def fit(self, X, Y, **kwargs):
-        super().fit(X, Y, **kwargs)
+    def _tune_threshold(self, X_val, Y_val, thin_thresholds=1):
+        scores = self.predict_scores(X_val)
+        probs = np.unique(scores)[::thin_thresholds]
+        threshold = 0.0
+        best = f1_score(Y_val, scores > threshold, average='micro')
+        for i, p in enumerate(probs):
+            pred = scores > p
+            f1 = f1_score(Y_val, pred, average='micro')
+            if f1 > best:
+                threshold = p
+                best = f1
+        self.logger.info('Tuned threshold, obtained {:.2f} which achieved'
+                         ' a micro F1-measure of {:.2f}'.format(
+            threshold, best))
+        return threshold
+
+    def fit(self, X, Y, tune_size=0.1, thin_thresholds=1, **kwargs):
+        if tune_size > 0:
+            X_train, X_val, Y_train, Y_val = train_test_split(
+                X, Y, test_size=tune_size)
+            try:
+                super().fit(X_train, Y_train, **kwargs)
+            finally:
+                self.logger.info('Fitting utility function finished.'
+                                 ' Start tuning threshold.')
+                self.threshold = self._tune_threshold(
+                    X_val, Y_val,
+                    thin_thresholds=thin_thresholds)
+        else:
+            super().fit(X, Y, **kwargs)
+            self.threshold = 0.5
 
     def predict_scores(self, X, **kwargs):
         return super().predict_scores(X, **kwargs)
 
     def predict(self, X, **kwargs):
         scores = self.predict_scores(X, **kwargs)
-        return scores > 0.5
+        return scores > self.threshold
