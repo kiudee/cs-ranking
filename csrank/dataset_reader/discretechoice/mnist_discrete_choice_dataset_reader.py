6f736ea8f94273c6ef901a7afc5e68902423a7c9
Pritha Gupta <prithag@mail.upb.de>
2018-05-07 17:12:33 +0200

    Added the discrete choice dataset reader for mnist

---
+++ csrank/dataset_reader/discretechoice/mnist_discrete_choice_dataset_reader.py
@@ -0,0 +1,61 @@
+import logging
+import os
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.utils import check_random_state
+
+from csrank.constants import CHOICE_FUNCTIONS
+from csrank.dataset_reader import DatasetReader
+
+
+class MNISTDiscreteChoiceDatasetReader(DatasetReader):
+
+    def __init__(self, n_train_instances=10000, n_test_instances=10000, n_objects=10, random_state=None, **kwargs):
+        super(MNISTDiscreteChoiceDatasetReader, self).__init__(learning_problem=CHOICE_FUNCTIONS,
+                                                               dataset_folder='mnist',
+                                                               **kwargs)
+        self.logger = logging.getLogger(MNISTDiscreteChoiceDatasetReader.__name__)
+        self.n_test_instances = n_test_instances
+        self.n_train_instances = n_train_instances
+        self.n_objects = n_objects
+        self.random_state = check_random_state(random_state)
+        self.n_features = None
+        self.__load_dataset__()
+
+    def __load_dataset__(self):
+        x_file = os.path.join(self.dirname, "X_raw_features.npy")
+        y_file = os.path.join(self.dirname, "y_labels.npy")
+        X_raw = np.load(x_file)
+        y_labels = np.load(y_file)
+        self.n_features = X_raw.shape[1]
+        num_classes = len(np.unique(y_labels))
+        n_total = self.n_test_instances + self.n_train_instances
+        largest_numbers = self.random_state.randint(1, num_classes, size=n_total)
+        self.X = np.empty((n_total, self.n_objects, self.n_features))
+        self.Y = np.empty(n_total, dtype=int)
+        for i in range(n_total):
+            remaining = X_raw[y_labels < largest_numbers[i]]
+            largest = X_raw[y_labels == largest_numbers[i]]
+            while True:
+                indices = self.random_state.choice(len(remaining), size=self.n_objects, replace=False)
+                ind = self.random_state.choice(len(largest), size=1)
+                choice = largest[ind]
+                self.X[i] = remaining[indices]
+                position = self.random_state.choice(self.n_objects, size=1)[0]
+                self.X[i][position] = choice[0]
+                self.Y[i] = position
+        self.__check_dataset_validity__()
+
+    def get_single_train_test_split(self):
+        return train_test_split(self.X, self.Y, random_state=self.random_state,
+                                test_size=self.n_test_instances)
+
+    def splitter(self, iter):
+        pass
+
+    def get_dataset_dictionaries(self, lengths=[5, 6]):
+        pass
+
+    def get_train_test_datasets(self, n_datasets=5):
+        pass
