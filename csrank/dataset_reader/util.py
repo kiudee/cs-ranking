d71aa34789c5d1bb7ae8bb265a68cd56d17d1257
prithag <prithag@mail.upb.de>
2018-05-11 16:13:07 +0200

    Re factored some methods from utils to dataset reader
    # Conflicts:
    #	csrank/__init__.py

--- csrank/dataset_reader/util.py
+++ csrank/dataset_reader/util.py
@@ -1,25 +1,87 @@
-class SyntheticIterator(object):
+import itertools as iter
 
-    def __init__(self, dataset_function, **params):
+import numpy as np
+
+def strongly_connected_components(graph):
+    """ Find the strongly connected components in a graph using
+        Tarjan's algorithm.
+        # Taken from http://www.logarithmic.net/pfh-files/blog/01208083168/sort.py
+
+        graph should be a dictionary mapping node names to
+        lists of successor nodes.
         """
-        Infinite iterator over a synthetic dataset generator.
 
-        Parameters
-        ----------
-        dataset_function : callable
-            Returns a tuple (inputs, targets) when called
-        params : dict
-            Parameters to be passed to `dataset_function` when called
-        """
-        self.params = params
-        self.func = dataset_function
+    result = []
+    stack = []
+    low = {}
 
-    def __iter__(self):
-        return self
+    def visit(node):
+        if node in low: return
 
-    def __next__(self):
-        return self.func(**self.params)
+        num = len(low)
+        low[node] = num
+        stack_pos = len(stack)
+        stack.append(node)
 
-    def __len__(self):
-        """Return a constant to allow for steps per epoch."""
-        return 100
+        for successor in graph[node]:
+            visit(successor)
+            low[node] = min(low[node], low[successor])
+
+        if num == low[node]:
+            component = tuple(stack[stack_pos:])
+            del stack[stack_pos:]
+            result.append(component)
+            for item in component:
+                low[item] = len(graph)
+
+    for node in graph:
+        visit(node)
+
+    return result
+
+
+def create_graph_pairwise_matrix(matrix):
+    n_objects = matrix.shape[0]
+    graph = {key: [] for key in np.arange(n_objects)}
+    for i, j in iter.combinations(np.arange(n_objects), 2):
+        p_ij = matrix[i, j]
+        p_ji = matrix[j, i]
+        if (p_ij > p_ji):
+            graph[j].append(i)
+        if (p_ij < p_ji):
+            graph[i].append(j)
+        if (p_ij == p_ji):
+            graph[j].append(i)
+            graph[i].append(j)
+    return graph
+
+
+def create_pairwise_prob_matrix(n_objects):
+    # Create a non-transitive pairwise probability matrix for n_objects*n_objects
+    non_transitive = False
+    while (not non_transitive):
+        pairwise_prob = np.zeros([n_objects, n_objects])
+        for i, j in iter.combinations(np.arange(n_objects), 2):
+            pairwise_prob[i, j] = np.random.rand(1)[0]
+            pairwise_prob[j, i] = 1.0 - pairwise_prob[i, j]
+        for comp in strongly_connected_components(create_graph_pairwise_matrix(pairwise_prob)):
+            if (len(comp) >= 3):
+                non_transitive = True
+                break
+    return pairwise_prob
+
+
+def quicksort(arr, matrix):
+    # Apply the quick sort algorithm for the given set of objects and produces an ordering based on provided pairwise matrix
+    if len(arr) < 2:
+        return arr
+    else:
+        print(arr)
+        pivot = np.argmax(np.sum(matrix, axis=1))
+        print(pivot)
+        arr.remove(pivot)
+        right = [i for i in arr if matrix[pivot, i] == 0]
+        left = [i for i in arr if matrix[pivot, i] == 1]
+        print("left {}".format(left))
+        print("right {}".format(right))
+        return quicksort(left, matrix) + [pivot] + quicksort(right, matrix)
