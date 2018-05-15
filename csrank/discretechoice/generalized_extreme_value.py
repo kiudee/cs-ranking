20727301b3bc76a446a134fd2d442c8c832ae632
prithag <prithag@mail.upb.de>
2018-05-15 16:22:50 +0200

    Added the first implementation of GEv model

---
+++ csrank/discretechoice/generalized_extreme_value.py
@@ -0,0 +1,108 @@
+import logging
+from itertools import combinations
+
+import numpy as np
+import pymc3 as pm
+import theano
+import theano.tensor as tt
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.utils import check_random_state
+
+from csrank.discretechoice.discrete_choice import DiscreteObjectChooser
+from csrank.discretechoice.likelihoods import likelihood_dict, LogLikelihood
+from csrank.tunable import Tunable
+from csrank.util import print_dictionary, softmax
+
+
+class GeneralizedNestedLogit(DiscreteObjectChooser, Tunable):
+
+    def __init__(self, n_features, n_objects, loss_function='', n_tune=500, n_sample=500, alpha=1e-3, random_state=None,
+                 **kwd):
+        self.n_tune = n_tune
+        self.n_sample = n_sample
+        self.n_features = n_features
+        self.n_objects = n_objects
+        self.nests_ind = np.array(list(combinations(np.arange(n_objects), 2)))
+        self.n_nests = len(self.nests_ind)
+        self.alpha = alpha
+        self.random_state = check_random_state(random_state)
+        self.logger = logging.getLogger(GeneralizedNestedLogit.__name__)
+        self.loss_function = likelihood_dict.get(loss_function, None)
+        self.model = None
+        self.trace = None
+
+    def get_probabilities(self, utility, lambda_k, alpha_ik, n_instances):
+        utility = tt.exp(utility)
+        utility_nest = tt.zeros((n_instances, self.n_objects, self.n_nests))
+        for i in range(self.n_nests):
+            uti = tt.power(utility * alpha_ik[:, i], 1 / lambda_k[i])
+            utility_nest = tt.set_subtensor(utility_nest[:, :, i], uti)
+        sum_per_nest = utility_nest.sum(axis=1)
+        denominators = tt.sum(tt.power(sum_per_nest, lambda_k), axis=1)
+        p = tt.zeros_like(utility)
+        for i in range(self.n_objects):
+            numerator_i = tt.power(sum_per_nest, (lambda_k - 1)) * utility_nest[:, i, :]
+            p = tt.set_subtensor(p[:, i], numerator_i.sum(axis=1) / denominators)
+        return p
+
+    def get_probabilities_np(self, utility, lambda_k, alpha_ik):
+        n_instances = utility.shape[0]
+        utility = np.exp(utility)
+        utility_nest = np.zeros((n_instances, self.n_objects, self.n_nests))
+        for i in range(self.n_nests):
+            uti = np.power(utility * alpha_ik[:, i], 1 / lambda_k[i])
+            utility_nest[:, :, i] = uti
+
+        sum_per_nest = utility_nest.sum(axis=1)
+        denominators = np.sum(np.power(sum_per_nest, lambda_k), axis=1)
+        p = np.zeros_like(utility)
+        for i in range(self.n_objects):
+            numerator_i = np.power(sum_per_nest, (lambda_k - 1)) * utility_nest[:, i, :]
+            p[:, i] = numerator_i.sum(axis=1) / denominators
+        return p
+
+    def fit(self, X, Y, **kwargs):
+        with pm.Model() as self.model:
+            mu_weights = pm.Normal('mu_weights', mu=0., sd=10)
+            sigma_weights = pm.HalfCauchy('sigma_weights', beta=1)
+            weights = pm.Normal('weights', mu=mu_weights, sd=sigma_weights, shape=self.n_features)
+            alpha_ik = pm.HalfNormal('alpha_ik', sd=1.0, shape=(self.n_objects, self.n_nests))
+            alpha_ik = tt.nnet.softmax(alpha_ik)
+            utility = pm.math.sum(weights * X, axis=2)
+            alpha = 1e-3
+            lambda_k = pm.Uniform('lambda_k', alpha, 1.0 + alpha, shape=self.n_nests)
+            p = self.get_probabilities(utility, lambda_k, alpha_ik, X.shape[0])
+
+            if self.loss_function is None:
+                yl = pm.Categorical('yl', p=p, observed=Y)
+                self.trace = pm.sample(self.n_sample, tune=self.n_tune, cores=8)
+            else:
+                Y = LabelBinarizer().fit_transform(Y)
+                Y = theano.shared(Y)
+                yl = LogLikelihood('yl', loss_func=self.loss_function, p=p, observed=Y)
+                self.trace = pm.sample(self.n_sample, tune=self.n_tune, cores=8)
+
+    def _predict_scores_fixed(self, X, **kwargs):
+        d = dict(pm.summary(self.trace)['mean'])
+        weights = np.array([d['weights__{}'.format(i)] for i in range(self.n_features)])
+        lambda_k = np.array([d['lambda_k__{}'.format(i)] for i in range(self.n_nests)])
+        alpha_ik = np.array(
+            [[d['alpha_ik__{}_{}'.format(i, k)] for k in range(self.n_nests)] for i in range(self.n_objects)])
+        alpha_ik = softmax(alpha_ik)
+        utility = np.sum(X * weights, axis=2)
+        p = self.get_probabilities_np(utility, lambda_k, alpha_ik)
+        return p
+
+    def predict(self, X, **kwargs):
+        return super().predict(X, **kwargs)
+
+    def predict_scores(self, X, **kwargs):
+        return super().predict_scores(X, **kwargs)
+
+    def set_tunable_parameters(self, n_tune=500, n_sample=500, alpha=1e-3, **point):
+        self.n_tune = n_tune
+        self.n_sample = n_sample
+        self.alpha = alpha
+        if len(point) > 0:
+            self.logger.warning('This ranking algorithm does not support tunable parameters'
+                                ' called: {}'.format(print_dictionary(point)))
