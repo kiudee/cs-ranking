19dcb744c0a718dc9ccb818940e7307d36fe8536
prithag <prithag@mail.upb.de>
2018-05-22 16:13:17 +0200

    Added new metrics for discrete choice problems
    Removed a bug in predictions for object ranker
    
    Remvoed logger from loss for dictionary
    
    # Conflicts:
    #	csrank/discretechoice/discrete_choice.py

--- csrank/discretechoice/discrete_choice.py
+++ csrank/discretechoice/discrete_choice.py
@@ -2,10 +2,10 @@ from abc import ABCMeta, abstractmethod
 
 from csrank.constants import DISCRETE_CHOICE
 
-__all__ = ['ObjectChooser']
+__all__ = ['DiscreteObjectChooser']
 
 
-class ObjectChooser(metaclass=ABCMeta):
+class DiscreteObjectChooser(metaclass=ABCMeta):
     @property
     def learning_problem(self):
         return DISCRETE_CHOICE
@@ -35,19 +35,97 @@ class ObjectChooser(metaclass=ABCMeta):
 
         Parameters
         ----------
-        X : array-like, shape (n_samples, n_objects, n_features)
+        X : dict or numpy array
+            Dictionary with a mapping from size of the choice set to numpy arrays
+            or a single numpy array of size:
+            (n_instances, n_objects, n_features)
 
 
         Returns
         -------
-        Y : array-like, shape (n_samples, n_objects)
-            Predicted choices
+        Y : dict or numpy array
+            Dictionary with a mapping from choice set  size to numpy arrays
+            or a single numpy array containing discrete choices of size:
+            (n_instances, 1)
         """
-        raise NotImplementedError
+
+        self.logger.debug('Predicting started')
+
+        scores = self.predict_scores(X, **kwargs)
+        self.logger.debug('Predicting scores complete')
+
+        return self.predict_for_scores(scores)
+
+    def predict_for_scores(self, scores):
+        """ Predict choices for a given collection scores for the sets of objects.
+
+        Parameters
+        ----------
+        scores : dict or numpy array
+            Dictionary with a mapping from choice set size to numpy arrays
+            or a single numpy array of size containing scores of each object of size:
+            (n_instances, n_objects)
+
+
+        Returns
+        -------
+        Y : dict or numpy array
+            Dictionary with a mapping from choice set  size to numpy arrays
+            or a single numpy array containing discrete choices of size:
+            (n_instances, 1)
+        """
+        if isinstance(scores, dict):
+            result = dict()
+            for n, s in scores.items():
+                result[n] = s.argmax(axis=1)
+
+        else:
+            result = scores.argmax(axis=1)
+        return result
 
     @abstractmethod
     def predict_scores(self, X, **kwargs):
-        """ Predict scores for a given collection of sets of objects.
+        """
+        Predict the latent utility scores for each object in X.
+
+        We need to distinguish several cases here:
+         * Predict with the non-variadic model on the same choice set size
+         * Predict with the non-variadic model on a new choice set size
+         * Predict with the variadic model on a known choice set size
+         * Predict with the variadic model on a new choice set size
+         * Predict on a variadic input
+
+        The simplest solution is creating (a) new model(s) in all of the cases,
+        even though it/they might already exist.
+
+         Parameters
+         ----------
+         X : dict or numpy array
+            Dictionary with a mapping from ranking size to numpy arrays
+            or a single numpy array of size:
+            (n_instances, n_objects, n_features)
+
+        Returns
+        -------
+        Y : dict or numpy array
+            Dictionary with a mapping from choice set size to numpy arrays
+            or a single numpy array of size:
+            (n_instances, n_objects)
+            Predicted scores
+        """
+        self.logger.info("Predicting scores")
+
+        if isinstance(X, dict):
+            scores = dict()
+            for ranking_size, x in X.items():
+                scores[ranking_size] = self._predict_scores_fixed(x, **kwargs)
+        else:
+            scores = self._predict_scores_fixed(X, **kwargs)
+        return scores
+
+    @abstractmethod
+    def _predict_scores_fixed(self, X, **kwargs):
+        """ Predict borda scores for a given collection of sets of objects.
 
         Parameters
         ----------
@@ -76,7 +154,7 @@ class ObjectChooser(metaclass=ABCMeta):
 
     @classmethod
     def __subclasshook__(cls, C):
-        if cls is ObjectChooser:
+        if cls is DiscreteObjectChooser:
             has_fit = any("fit" in B.__dict__ for B in C.__mro__)
             has_predict = any("predict" in B.__dict__ for B in C.__mro__)
             has_scores = any("predict_scores" in B.__dict__ for B in C.__mro__)
--- csrank/objectranking/object_ranker.py
+++ csrank/objectranking/object_ranker.py
@@ -50,25 +50,24 @@ class ObjectRanker(metaclass=ABCMeta):
         -------
         Y : dict or numpy array
             Dictionary with a mapping from ranking size to numpy arrays
-            or a single numpy array of size:
+            or a single numpy array containing predicted ranking of size:
             (n_instances, n_objects)
-            Predicted ranking
         """
         self.logger.debug('Predicting started')
 
         scores = self.predict_scores(X, **kwargs)
         self.logger.debug('Predicting scores complete')
 
-        return self.predict_for_scores(scores)
+        return self.predict_for_scores(scores, **kwargs)
 
     def predict_for_scores(self, scores, **kwargs):
-        """ Predict rankings for a given collection of sets of objects.
+        """ Predict rankings for scores for a given collection of sets of objects.
 
         Parameters
         ----------
         scores : dict or numpy array
             Dictionary with a mapping from ranking size to numpy arrays
-            or a single numpy array of size containing scores of each object:
+            or a single numpy array of size containing scores of each object of size:
             (n_instances, n_objects)
 
 
@@ -76,15 +75,14 @@ class ObjectRanker(metaclass=ABCMeta):
         -------
         Y : dict or numpy array
             Dictionary with a mapping from ranking size to numpy arrays
-            or a single numpy array of size:
+            or a single numpy array containing predicted ranking of size:
             (n_instances, n_objects)
-            Predicted ranking
         """
 
         if isinstance(scores, dict):
             result = dict()
             for n, score in scores.items():
-                rankings = scores_to_rankings(scores)
+                rankings = scores_to_rankings(score)
                 result[n] = rankings
         else:
             result = scores_to_rankings(scores)
