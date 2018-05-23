d4191611d95419372ee7c241e225c1b836ce5238
prithag <prithag@mail.upb.de>
2018-05-23 16:00:51 +0200

    Made nests for generalized extreme value models tunnable

--- csrank/discretechoice/__init__.py
+++ csrank/discretechoice/__init__.py
@@ -1,6 +1,6 @@
 from .fate_discrete_choice import FATEDiscreteChoiceFunction
 from .feta_discrete_choice import FETADiscreteChoiceFunction
-from .generalized_extreme_value import GeneralizedNestedLogit
+from .generalized_extreme_value import GeneralizedExtremeValueModel
 from .multinomial_logit_model import MultinomialLogitModel
 from .nested_logit_model import NestedLogitModel
 from .paired_combinatorial_logit import PairedCombinatorialLogit
--- csrank/discretechoice/generalized_extreme_value.py
+++ csrank/discretechoice/generalized_extreme_value.py
@@ -1,5 +1,4 @@
 import logging
-from itertools import combinations
 
 import numpy as np
 import pymc3 as pm
@@ -12,19 +11,22 @@ from .discrete_choice import DiscreteObjectChooser
 from .likelihoods import likelihood_dict, LogLikelihood
 
 
-class GeneralizedNestedLogit(DiscreteObjectChooser, Tunable):
+class GeneralizedExtremeValueModel(DiscreteObjectChooser, Tunable):
 
-    def __init__(self, n_features, n_objects, loss_function='', n_tune=500, n_sample=500, alpha=1e-3, random_state=None,
+    def __init__(self, n_features, n_objects, n_nests=None, loss_function='None', n_tune=500, n_sample=500, alpha=1e-3,
+                 random_state=None,
                  **kwd):
         self.n_tune = n_tune
         self.n_sample = n_sample
         self.n_features = n_features
         self.n_objects = n_objects
-        self.nests_ind = np.array(list(combinations(np.arange(n_objects), 2)))
-        self.n_nests = len(self.nests_ind)
+        if n_nests is None:
+            self.n_nests = n_objects + int(n_objects / 2)
+        else:
+            self.n_nests = n_nests
         self.alpha = alpha
         self.random_state = check_random_state(random_state)
-        self.logger = logging.getLogger(GeneralizedNestedLogit.__name__)
+        self.logger = logging.getLogger(GeneralizedExtremeValueModel.__name__)
         self.loss_function = likelihood_dict.get(loss_function, None)
         self.model = None
         self.trace = None
@@ -96,10 +98,14 @@ class GeneralizedNestedLogit(DiscreteObjectChooser, Tunable):
     def predict_scores(self, X, **kwargs):
         return super().predict_scores(X, **kwargs)
 
-    def set_tunable_parameters(self, n_tune=500, n_sample=500, alpha=1e-3, **point):
+    def set_tunable_parameters(self, n_tune=500, n_sample=500, alpha=1e-3, n_nests=None, **point):
         self.n_tune = n_tune
         self.n_sample = n_sample
         self.alpha = alpha
+        if n_nests is None:
+            self.n_nests = self.n_objects + int(self.n_objects / 2)
+        else:
+            self.n_nests = n_nests
         if len(point) > 0:
             self.logger.warning('This ranking algorithm does not support tunable parameters'
                                 ' called: {}'.format(print_dictionary(point)))
