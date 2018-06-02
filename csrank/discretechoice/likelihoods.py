03035d2aaaed6bd962c471e9ced7fc70ebf2fc93
prithag <prithag@mail.upb.de>
2018-06-02 12:40:14 +0200

    Removed bugs in likelihood functions

--- csrank/discretechoice/likelihoods.py
+++ csrank/discretechoice/likelihoods.py
@@ -1,11 +1,13 @@
 import theano
 import theano.tensor as tt
-from pymc3 import Categorical
+from pymc3 import Discrete
 from pymc3.distributions.dist_math import bound
+
+from csrank.util import normalize
 
 
 def categorical_crossentropy(p, y_true):
-    return tt.nnet.categorical_crossentropy(p, y_true)
+    return -tt.nnet.categorical_crossentropy(p, y_true)
 
 
 def binary_crossentropy(p, y_true):
@@ -13,20 +15,20 @@ def binary_crossentropy(p, y_true):
         l = tt.nnet.binary_crossentropy(p, y_true).mean(axis=1)
     else:
         l = tt.nnet.binary_crossentropy(p, y_true).mean(axis=0)
-    return l
+    return -l
 
 
 def categorical_hinge(p, y_true):
     pos = tt.sum(y_true * p, axis=-1)
     neg = tt.max((1. - y_true) * p, axis=-1)
-    return tt.maximum(0., neg - pos + 1.)
+    return -tt.maximum(0., neg - pos + 1.)
 
 
 likelihood_dict = {'categorical_crossentropy': categorical_crossentropy, 'binary_crossentropy': binary_crossentropy,
                    'categorical_hinge': categorical_hinge}
 
 
-class LogLikelihood(Categorical):
+class LogLikelihood(Discrete):
     R"""
     Categorical log-likelihood.
 
@@ -45,17 +47,23 @@ class LogLikelihood(Categorical):
         rescaled otherwise.
     """
 
-    def __init__(self, loss_func, *args, **kwargs):
+    def __init__(self, loss_func, p, *args, **kwargs):
         super(LogLikelihood, self).__init__(*args, **kwargs)
         self.loss_func = loss_func
+        try:
+            self.k = tt.shape(p)[-1].tag.test_value
+        except AttributeError:
+            self.k = tt.shape(p)[-1]
+        self.p = tt.as_tensor_variable(p)
+        self.mode = tt.argmax(p)
 
     def random(self, point=None, size=None, repeat=None):
-        return super().random(point, size, repeat)
+        return NotImplemented
 
     def logp(self, value):
         p = self.p
-        # Clip values before using them for indexing
+        a = self.loss_func(p, value)
+        p = normalize(p)
         sumto1 = theano.gradient.zero_grad(
             tt.le(abs(tt.sum(p, axis=-1) - 1), 1e-5))
-        a = -self.loss_func(p, value)
         return bound(a, sumto1)
