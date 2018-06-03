14024c1fb1bad1cd10c5f01d786e3a7c59d184cc
prithag <prithag@mail.upb.de>
2018-06-03 22:53:00 +0200

    result script

--- experiments/result_script.py
+++ experiments/result_script.py
@@ -3,9 +3,9 @@ import logging
 import os
 
 import pandas as pd
-import tabulate
+from tabulate import tabulate
 
-from csrank.util import configure_logging_numpy_keras
+from csrank.util import configure_logging_numpy_keras, print_dictionary
 from experiments.dbconnection import DBConnector
 
 if __name__ == '__main__':
@@ -18,32 +18,25 @@ if __name__ == '__main__':
     self.init_connection()
     results_table = "results.object_ranking"
     avail_jobs = "{}.avail_jobs".format(self.schema)
-    select_st = "SELECT dataset_params, dataset, kendallstau, spearmancorrelation, zerooneranklossties, zerooneaccuracy, ndcgtopall  from {0} INNER JOIN {1} ON {0}.job_id = {1}.job_id where {1}.dataset='letor_or'".format(
+    select_st = "SELECT dataset_params, dataset, learner, kendallstau, spearmancorrelation, zerooneaccuracy, ndcgtopall, zerooneranklossties  from {0} INNER JOIN {1} ON {0}.job_id = {1}.job_id where {1}.dataset='letor_or'".format(
         results_table, avail_jobs)
     self.cursor_db.execute(select_st)
-    data_m = []
-    data_h = []
-    data_d = []
+    data_2007 = []
+    data_2008 = []
     for job in self.cursor_db.fetchall():
         values = list(job.values())
         keys = list(job.keys())
         columns = keys[2:]
-        if job['dataset_params']['dataset_type'] == 'medoid':
-            data_m.append(values[2:])
-        if job['dataset_params']['dataset_type'] == 'hypervolume':
-            data_h.append(values[2:])
-        if job['dataset_params']['dataset_type'] == 'basic':
-            data_d.append(values[2:])
+        vals = values[2:] + [1.0 - values[-1]]
+        if job['dataset_params']['year'] == 2007:
+            values_ = ['Letor2007'] + vals
+            data_2007.append(values_)
+        if job['dataset_params']['year'] == 2008:
+            values_ = ['Letor2008'] + vals
+            data_2008.append(values_)
     cols = ['Dataset'] + columns + ['zeroonerankaccuracy']
-    data_com = []
-    for name, data in zip(['Medoid', 'HyperVolume', 'DepthBasic'], [data_m, data_h, data_d]):
-        one_row = [name]
-        df = pd.DataFrame(data, columns=columns)
-        df['zeroonerankaccuracy'] = 1.0 - df["zerooneranklossties"]
-        std = df.std(axis=0).values
-        mean = df.mean(axis=0).values
-        one_row.extend(["{:.3f}+-{:.3f}".format(m, s) for m, s in zip(mean, std)])
-        data_com.append(one_row)
-    df = pd.DataFrame(data_com, columns=cols)
-    df.to_csv('complete.csv')
+    df = pd.DataFrame(data_2008, columns=cols)
+    df.to_csv('2008.csv')
+    df = pd.DataFrame(data_2007, columns=cols)
+    df.to_csv('2007.csv')
     print(tabulate(df))
