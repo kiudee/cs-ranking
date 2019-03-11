7d5671af4a2d1331edaa848d5cc3139e9a5f1889
prithag <prithag@mail.upb.de>
2019-03-11 19:44:04 +0100

    Added letor choice dataset parser

--- csrank/dataset_reader/choicefunctions/__init__.py
+++ csrank/dataset_reader/choicefunctions/__init__.py
@@ -1,2 +1,3 @@
 from .choice_data_generator import ChoiceDatasetGenerator
 from .mnist_choice_dataset_reader import MNISTChoiceDatasetReader
+from .letor_ranking_choice_dataset import LetorRankingChoiceDatasetReader
--- csrank/dataset_reader/discretechoice/letor_ranking_discrete_choice_dataset_reader.py
+++ csrank/dataset_reader/discretechoice/letor_ranking_discrete_choice_dataset_reader.py
@@ -3,7 +3,7 @@ import logging
 from sklearn.utils import check_random_state
 
 from csrank.constants import DISCRETE_CHOICE
-from .util import convert_to_label_encoding, sub_sampling_discrete_choices_from_relevance
+from .util import sub_sampling_discrete_choices_from_relevance
 from ..letor_ranking_dataset_reader import LetorRankingDatasetReader
 
 
@@ -17,10 +17,6 @@ class LetorRankingDiscreteChoiceDatasetReader(LetorRankingDatasetReader):
 
     def sub_sampling_function(self, X, Y):
         return sub_sampling_discrete_choices_from_relevance(Xt=X, Yt=Y, n_objects=self.n_objects)
-
-    def convert_output(self, ranking_length):
-        self.Y = self.Y.argmin(axis=1)
-        self.Y = convert_to_label_encoding(self.Y, ranking_length)
 
     def splitter(self, iter):
         pass
