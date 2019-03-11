0b6c676991e1d85c2f4b14cdf464049e16ca36a7
prithag <prithag@mail.upb.de>
2019-03-11 19:44:04 +0100

    Choice Functions  - LETOR Choice dataset parser  - Baseline for Choice functions

--- csrank/choicefunctions/__init__.py
+++ csrank/choicefunctions/__init__.py
@@ -6,3 +6,4 @@ from .feta_choice import FETAChoiceFunction
 from .generalized_linear_model import GeneralizedLinearModel
 from .pairwise_choice import PairwiseSVMChoiceFunction
 from .ranknet_choice import RankNetChoiceFunction
+from .baseline import AllPositive
