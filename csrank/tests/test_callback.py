03bdb1090d67b1ecd5ad402f9fa411f3b8b8036e
Pritha Gupta <prithagupta.nsit@gmail.com>
2019-10-21 23:59:37 +0200

    Issue #58 Made Early Stopping independent of the Early Stopping from the keras
    Wrote the test for EarlyStopping callback

--- csrank/tests/test_callback.py
+++ csrank/tests/test_callback.py
@@ -1,12 +1,15 @@
 import math
+import os
 
 import numpy as np
+import tensorflow as tf
 from keras import Sequential
 from keras import backend as K
 from keras.layers import Dense
 from keras.optimizers import SGD
 
-from csrank.callbacks import LRScheduler
+from csrank.callbacks import LRScheduler, EarlyStoppingWithWeights
+from csrank.tests.test_ranking import check_params_tunable
 
 lr = 0.015
 model = Sequential()
@@ -14,6 +17,13 @@ model.add(Dense(10, activation='relu'))
 model.add(Dense(5, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(optimizer=SGD(lr=lr), loss='binary_crossentropy')
+patience = 5
+min_delta = 1e-2
+early_stop = EarlyStoppingWithWeights(min_delta=min_delta, patience=patience)
+epochs_drop = 5
+drop = 0.9
+lr_scheduler = LRScheduler(epochs_drop=epochs_drop, drop=drop)
+callbacks = [lr_scheduler, early_stop]
 
 
 def sigmoid(x):
@@ -29,16 +39,23 @@ def trivial_classification_problem():
     return x, y_true
 
 
-def test_lr_scheduler():
+def test_callbacks():
+    tf.set_random_seed(0)
+    os.environ["KERAS_BACKEND"] = "tensorflow"
+    np.random.seed(123)
     x, y = trivial_classification_problem()
-    epochs_drop = 5
-    drop = 0.9
-    epochs = 10
-    callbacks = [LRScheduler(epochs_drop=epochs_drop, drop=drop)]
-    model.fit(x, y, epochs=epochs, callbacks=callbacks)
+    epochs = 30
+    model.fit(x, y, epochs=epochs, callbacks=callbacks, validation_split=0.1)
     rtol = 1e-2
-    atol = 5e-2
-    step = math.floor((epochs) / epochs_drop)
+    atol = 5e-4
+    step = math.floor(early_stop.stopped_epoch / epochs_drop)
     actual_lr = lr * math.pow(drop, step)
     n_lr = K.get_value(model.optimizer.lr)
     assert np.isclose(actual_lr, n_lr, rtol=rtol, atol=atol, equal_nan=False)
+    params = {"epochs_drop": 100, "drop": 0.5}
+    lr_scheduler.set_tunable_parameters(**params)
+    check_params_tunable(lr_scheduler, params, rtol, atol)
+    assert early_stop.stopped_epoch == 10
+    params = {"patience": 10, "min_delta": 1e-5}
+    early_stop.set_tunable_parameters(**params)
+    check_params_tunable(early_stop, params, rtol, atol)
