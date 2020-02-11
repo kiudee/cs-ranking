5e8115dc4221eadc637187ee08a489b66d98f85c
Karlson Pfannschmidt <kiudee@mail.upb.de>
2020-02-11 11:10:29 +0100

    Set up cs-ranking for release on PyPi (#84)
    * Set up cs-ranking for proper versioning
    
    The following changes are implemented:
    
    * Usage of bump2version to semi-automated changing of version strings
    * Add token for PyPi to .travis.yml
    * Create HISTORY.rst file to track changes
    * Bump version: 1.0.0 → 1.0.1

--- setup.py
+++ setup.py
@@ -2,25 +2,33 @@ from os.path import realpath, dirname, join
 
 from setuptools import setup, find_packages
 
-DISTNAME = 'csrank'
-DESCRIPTION = 'Context-sensitive ranking'
-MAINTAINER = 'Karlson Pfannschmidt'
-MAINTAINER_EMAIL = 'kiudee@mail.upb.de'
-VERSION = "1.0"
+with open("README.rst") as readme_file:
+    readme = readme_file.read()
+
+with open("HISTORY.rst") as history_file:
+    history = history_file.read()
+
+DISTNAME = "csrank"
+DESCRIPTION = "Context-sensitive ranking"
+MAINTAINER = "Karlson Pfannschmidt"
+MAINTAINER_EMAIL = "kiudee@mail.upb.de"
 
 PROJECT_ROOT = dirname(realpath(__file__))
-REQUIREMENTS_FILE = join(PROJECT_ROOT, 'requirements.txt')
+REQUIREMENTS_FILE = join(PROJECT_ROOT, "requirements.txt")
 
 with open(REQUIREMENTS_FILE) as f:
     install_reqs = f.read().splitlines()
 
 if __name__ == "__main__":
-    setup(name=DISTNAME,
-          version=VERSION,
-          maintainer=MAINTAINER,
-          maintainer_email=MAINTAINER_EMAIL,
-          description=DESCRIPTION,
-          packages=find_packages(),
-          install_requires=install_reqs,
-          package_data={'notebooks': ['*']},
-          include_package_data=True)
+    setup(
+        name=DISTNAME,
+        version="1.0.1",
+        maintainer=MAINTAINER,
+        maintainer_email=MAINTAINER_EMAIL,
+        description=DESCRIPTION,
+        packages=find_packages(),
+        install_requires=install_reqs,
+        package_data={"notebooks": ["*"]},
+        include_package_data=True,
+        long_description=readme + "\n\n" + history
+    )
