52998f5d3104d9edb7d66d7cd3484782aad87bd9
Timo Kaufmann <timokau@zoho.com>
2020-03-19 17:56:21 +0100

    Remove the experiments module
    Following the removal of the experiments themselves. All functionality
    that is used elsewhere has been moved elsewhere.

--- csrank/__init__.py
+++ csrank/__init__.py
@@ -4,7 +4,6 @@ from .choicefunction import *
 from .core import *
 from .dataset_reader import *
 from .discretechoice import *
-from .experiments import *
 from .objectranking import *
 from .tunable import Tunable
 from .tuning import ParameterOptimizer
