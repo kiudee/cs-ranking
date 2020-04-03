071fe1efadcdc10a62ddf2ccb930175de1240e77
Timo Kaufmann <timokau@zoho.com>
2020-04-03 16:10:22 +0200

    Use defs instead of named lambdas
    Leads to more readable tracebacks:
    https://www.flake8rules.com/rules/E731.html

--- csrank/core/feta_network.py
+++ csrank/core/feta_network.py
@@ -240,9 +240,11 @@ class FETANetwork(Learner):
             outputs[j].append(n_l)
         # convert rows of pairwise matrix to keras layers:
         outputs = [concatenate(x) for x in outputs]
+
         # compute utility scores:
-        sum_func = lambda s: K.mean(s, axis=1, keepdims=True)
-        scores = [Lambda(sum_func)(x) for x in outputs]
+        scores = [
+            Lambda(lambda s: K.mean(s, axis=1, keepdims=True))(x) for x in outputs
+        ]
         scores = concatenate(scores)
         self.logger.debug("1st order model finished")
         if self._use_zeroth_model:
