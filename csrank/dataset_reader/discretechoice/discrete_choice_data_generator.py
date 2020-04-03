kiudee/cs-ranking

commit 1ec2ff7bcb0772d112e668b2a655598d314d1112

Make pygmo optional

csrank/dataset_reader/discretechoice/discrete_choice_data_generator.py
@@ -1,5 +1,12 @@
 import numpy as np
-from pygmo import hypervolume
+
+try:
+    import pygmo
+except ImportError:
+    from csrank.util import MissingExtraError
+
+    raise MissingExtraError("pygmo", "data")
+
 from scipy.spatial.distance import squareform, pdist
 from sklearn.datasets import make_regression, make_blobs
 from sklearn.gaussian_process.kernels import Matern

csrank/dataset_reader/objectranking/object_ranking_data_generator.py
@@ -1,5 +1,12 @@
 import numpy as np
-from pygmo import hypervolume
+
+try:
+    import pygmo
+except ImportError:
+    from csrank.util import MissingExtraError
+
+    MissingExtraError("pygmo", "data")
+
 from scipy.spatial.distance import pdist, squareform
 from sklearn.datasets import make_regression
 from sklearn.datasets.samples_generator import make_blobs

setup.py
@@ -46,6 +46,7 @@ if __name__ == "__main__":
                 "psycopg2-binary>=2.7",  # database access
                 "pandas>=0.22",
                 "h5py>=2.7",
+                "pygmo>=2.7",
             ],
         },
         package_data={"notebooks": ["*"]},

commit b031340483bf21f4bf7cbba0c3b1b68fe92c7c7a

Fix undefined hypervolume references
This fixes an error I introduced in
1ec2ff7bcb0772d112e668b2a655598d314d1112 and made worse in
3643ab2a352ae30267ec2aac31f60185012e6585. Discovered by flake8's F821.

csrank/dataset_reader/discretechoice/discrete_choice_data_generator.py
@@ -1,5 +1,13 @@
 import numpy as np
 
+try:
+    from pygmo import hypervolume
+except ImportError:
+    from csrank.util import MissingExtraError
+
+    raise MissingExtraError("pygmo", "data")
+
+
 from scipy.spatial.distance import squareform, pdist
 from sklearn.datasets import make_regression, make_blobs
 from sklearn.gaussian_process.kernels import Matern

csrank/dataset_reader/objectranking/object_ranking_data_generator.py
@@ -1,5 +1,13 @@
 import numpy as np
 
+try:
+    from pygmo import hypervolume
+except ImportError:
+    from csrank.util import MissingExtraError
+
+    raise MissingExtraError("pygmo", "data")
+
+
 from scipy.spatial.distance import pdist, squareform
 from sklearn.datasets import make_regression
 from sklearn.datasets.samples_generator import make_blobs

setup.py
@@ -56,6 +56,7 @@ if __name__ == "__main__":
                 "psycopg2-binary>=2.7",  # database access
                 "pandas>=0.22",
                 "h5py>=2.7",
+                "pygmo>=2.7",
             ],
             "probabilistic": ["pymc3>=3.8", "theano>=1.0"],
         },