454073ec5f1bd46df9b411c35b25228f8455c8d4
Timo Kaufmann <timokau@zoho.com>
2020-09-18 16:53:12 +0200

    Fix random state handling in the baseline

--- csrank/objectranking/baseline.py
+++ csrank/objectranking/baseline.py
@@ -16,7 +16,7 @@ class RandomBaselineRanker(ObjectRanker, Learner):
             :param kwargs: Keyword arguments for the algorithms
         """
 
-        self.random_state = (random_state,)
+        self.random_state = random_state
 
     def _pre_fit(self):
         super()._pre_fit()
