63cd4de9ca86c4334e63d92091da9886c7957849
Karlson Pfannschmidt <kiudee@mail.upb.de>
2021-03-18 14:20:27 +0100

    Fix activation layer for SDAChoiceFunction

--- csrank/choicefunction/sda_choice.py
+++ csrank/choicefunction/sda_choice.py
@@ -1,5 +1,5 @@
 from keras import Model
-from keras.layers import Dense, Input
+from keras.layers import Activation, Input
 
 from csrank.choicefunction.choice_functions import ChoiceFunctions
 from csrank.core.sda_core import SDACore
@@ -44,21 +44,11 @@ class SDAChoiceFunction(SDACore, ChoiceFunctions):
         )
         self.threshold = threshold
 
-    def _construct_layers(self, **kwargs):
-        super()._construct_layers(**kwargs)
-        self.scorer = Dense(
-            units=1,
-            name="output_node",
-            activation="sigmoid",
-            kernel_regularizer=self.regularization_strength,
-        )
-
     def construct_model(self, n_features, n_objects):
         model = super().construct_model(n_features=n_features, n_objects=n_objects)
         input_layer = Input(shape=(n_objects, n_features), name="input_node_outer")
-        scores = model(input_layer)
-        sigmoid_scores = self.scorer(scores)
-        final_model = Model(inputs=input_layer, outputs=sigmoid_scores)
+        scores = Activation("sigmoid")(model(input_layer))
+        final_model = Model(inputs=input_layer, outputs=scores)
         final_model.compile(
             loss=self.loss_function, optimizer=self.optimizer, metrics=self.metrics
         )
