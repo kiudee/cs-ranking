172722e798a8d9f6a122e4eada156396e7428ecc
Timo Kaufmann <timokau@zoho.com>
2021-04-03 23:57:05 +0200

    Mark poetry2nix setup as broken
    A hotfix is available, but not committed to the repository. See the
    comment in `shell.nix` for more details.

--- shell.nix
+++ shell.nix
@@ -25,7 +25,12 @@ let
   };
 in pkgs.mkShell {
   buildInputs = [
-    pythonEnv
+    # Unfortunately the poetry2nix setup is currently broken due to an issue
+    # with newer versions of matplotlib. If you need it, you can temporarily
+    # downgrade matplotlib  by executing `poetry add 'matplotlib<3.4'`. You can
+    # then uncomment the `pythonEnv`. See [1] for more information.
+    # [1] https://github.com/nix-community/poetry2nix/issues/280#issuecomment-815064853
+    # pythonEnv
     pkgs.python37.pkgs.poetry
   ];
 }
