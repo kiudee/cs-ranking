2535707818b53f345bdb8385882e1209029b3b41
Karlson Pfannschmidt <kiudee@mail.upb.de>
2021-04-12 14:16:35 +0200

    Apply zimports on __init__.py

--- csrank/__init__.py
+++ csrank/__init__.py
@@ -1,12 +1,12 @@
 from importlib.metadata import version
 
-# We should re-evaluate if we really want to re-export everything here and then
-# use __all__ properly.
-
 from .choicefunction import *  # noqa
 from .core import *  # noqa
 from .dataset_reader import *  # noqa
 from .discretechoice import *  # noqa
 from .objectranking import *  # noqa
+
+# We should re-evaluate if we really want to re-export everything here and then
+# use __all__ properly.
 
 __version__ = version(__name__)
