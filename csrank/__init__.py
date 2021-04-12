563a63549112cd5ef4308e8351912a3c75bf0acc
Karlson Pfannschmidt <kiudee@mail.upb.de>
2021-04-12 13:29:40 +0200

    Use importlib.metadata to automatically fetch the package version

--- csrank/__init__.py
+++ csrank/__init__.py
@@ -1,10 +1,12 @@
-__version__ = "1.2.1"
-
-# We should re-evaluate if we really want to re-export everything here and then
-# use __all__ properly.
+from importlib.metadata import version
 
 from .choicefunction import *  # noqa
 from .core import *  # noqa
 from .dataset_reader import *  # noqa
 from .discretechoice import *  # noqa
 from .objectranking import *  # noqa
+
+# We should re-evaluate if we really want to re-export everything here and then
+# use __all__ properly.
+
+__version__ = version(__name__)
