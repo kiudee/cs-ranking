55df30512ead91d28579a78c26c5854003ae4695
Karlson Pfannschmidt <kiudee@mail.upb.de>
2021-05-04 18:28:23 +0200

    Let nox install the data dependencies for testing

--- noxfile.py
+++ noxfile.py
@@ -63,7 +63,7 @@ def activate_virtualenv_in_precommit_hooks(session):
 @session(python=python_versions)
 def tests(session):
     """Run the test suite."""
-    session.install(".")
+    session.install(".[data]")
     session.install("coverage[toml]", "pytest", "nox", "nox-poetry")
     try:
         session.run("coverage", "run", "--parallel", "-m", "pytest", *session.posargs)
